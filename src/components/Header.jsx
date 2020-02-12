159ccf9bdec7ac1d3bcb242153ea44227921a150
Student <example@example.com>
2020-02-12 11:45:12 -0800

    add styles to navbar and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import NavLinks from './NavLinks'
 
 function Header(){
+  var headerStyles = {
+    margin: '0 0 0 50px',
+    paddingTop: '15px'
+  }
   return (
-    <div>
+    <div style={headerStyles}>
       <NavLinks/>
     </div>
   )
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function NavLinks() {
+  var navBarStyles ={
+    fontFamily: 'HeiT ASC Traditional Chinese,M Hei PRC W45,AXIS Font Japanese W55,FB New Gothic,Gotham Medium,system,sans-serif',
+    textTransform: 'uppercase',
+    textAlign: 'center'
+  }
+  var teslaLogo ={
+    float: 'left'
+  }
+  var navMargins ={
+    margin: '0 10px'
+  }
   return (
-      <div>
-        <Link to="/">Home</Link> | <Link to="/models">Model S</Link> | <Link to="/model3">Model 3</Link> | <Link to="/modelx">Model X</Link> | <Link to="/cybertruck">Create Ticket</Link> | <Link to="/roadster">Roadster</Link> | <Link to="/energy">Energy</Link> 
+      <div style={navBarStyles}>
+        <Link style={teslaLogo}  to="/">TESLA</Link>  <Link style={navMargins} to="/models">Model S</Link>  <Link style={navMargins} to="/model3">Model 3</Link>  <Link style={navMargins} to="/modelx">Model X</Link>  <Link style={navMargins} to="/cybertruck">Cybertruck </Link>  <Link style={navMargins} to="/roadster">Roadster</Link>  <Link style={navMargins} to="/energy">Energy</Link>
       </div>
     );
   }
