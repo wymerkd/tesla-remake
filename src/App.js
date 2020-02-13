15bb33228ac17bef501b10a4f16c447008f2769c
Student <example@example.com>
2020-02-12 16:23:04 -0800

    add error404 page/route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ModelS from './components/ModelS';
+import Error404 from './components/Error404';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/models' component={ModelS}/>
+        <Route component={Error404}/>
       </Switch>
     </div>
   );
